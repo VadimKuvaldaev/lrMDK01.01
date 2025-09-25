70b8108f63f0fddaee7c29fd2a2a99932193aa10
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-09-25 08:59:31 +0300

    Сделан массив, который вводится с клавиатуры, а также подсчитывается среднее значение его элементов.

--- LAB1/LAB1/Program.cs
+++ LAB1/LAB1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,12 +19,14 @@ namespace LAB1
             for (int i = 0; i < Length; i++)
             {
                 Array[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            Console.WriteLine("Элементы массива: ");
-            foreach(int element in Array)
+            }            
+            int sum = 0;  
+            foreach(int i in Array) 
             {
-                Console.WriteLine(element);
+                sum += i;
             }
+            double avarage = (double)sum / Length; 
+            Console.WriteLine($"Среднее значение: {avarage}");
         }
     }
 }
