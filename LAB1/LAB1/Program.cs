2c435fc70f3d211ff5494eedaee6a01dd0a9304c
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2025-10-10 17:35:20 +0300

    Создан массив, котрый пользователь вводит с клавиатуры

---
+++ LAB1/LAB1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB1
+{
+    internal class Program
+    {
+        /// Кувалдаев Вариант_2 Задание_1
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива: ");
+            int Length = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[Length];
+            Console.WriteLine("Введите элементы массива");
+            for (int i =0; i < Length; i++)
+            {
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
+    }
+}
