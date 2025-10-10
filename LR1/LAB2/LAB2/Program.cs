cf8c350c815bb1eb45af5d8f5cf098837a34117d
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2025-10-10 18:51:47 +0300

    Создан массив, котрый заполняется числами от 10 до 2000

---
+++ LR1/LAB2/LAB2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2
+{
+    internal class Program
+    {
+        /// Кувалдаев Вариант_2 Задание_2
+        static void Main(string[] args)
+        {
+            int[] array = new int[1990];
+            int a = 10;
+            for (int i = 0; i < array.Length; i++) 
+            {
+              array[i] = a;
+                ++a;
+            }
+            
+        }
+    }
+}
