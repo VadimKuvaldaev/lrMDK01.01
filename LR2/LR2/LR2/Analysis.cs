1143882f17e452216aff8f89c7663648cbab7351
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2025-11-10 18:24:44 +0300

    Реализована функция сортировки игр по количеству скачиваний

--- LR2/LR2/LR2/Analysis.cs
+++ LR2/LR2/LR2/Analysis.cs
@@ -10,7 +10,22 @@ namespace LR2
     {
         static public void SortGames(List<string> games, List<int> counts) // Сортировка игр по количеству скачиваний
         {
-
+            for (int i = 0; i < counts.Count; i++)
+            {
+                for (int j = 0; j < counts.Count - 1; j++)
+                {
+                    bool condition = counts[j + 1] > counts[j];
+                    if (condition)
+                    {
+                        int temp_counts = counts[j];
+                        counts[j] = counts[j + 1];
+                        counts[j + 1] = temp_counts;
+                        string temp_books = games[j];
+                        games[j] = games[j + 1];
+                        games[j + 1] = temp_books;
+                    }
+                }
+            }
         }
     }
 }
