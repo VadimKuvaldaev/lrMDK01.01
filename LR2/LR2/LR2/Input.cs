34ea33e3115439d58ae2927f4e738a76933058db
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2025-11-10 17:36:43 +0300

    Создана функция, в которой будут данные о играх и их жанрах, а также количество их скачиваний

---
+++ LR2/LR2/LR2/Input.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class Input
+    {
+        static public (List<string>[], List<int>[]) InputGamesByGenre() 
+        { 
+          
+        }
+    }
+}
