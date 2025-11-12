9fe9d8ad4134786459ed08502fd3698a08097e29
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2025-11-12 19:17:21 +0300

    Переработана функция AddSale и удалена функция инициализации цен

--- DistanceLearning2/DistanceLearning2/SalesFunctions.cs
+++ DistanceLearning2/DistanceLearning2/SalesFunctions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,38 +10,25 @@ namespace DistanceLearning2
 {
     public class SalesFunctions
     {
-        public static Dictionary<string, double> InitializePhonePrices() // Функция инициализации цен
-        {
-            Dictionary<string, double> phonePrices = new Dictionary<string, double>(); 
-            phonePrices.Add("iPhone 15", 999.99); 
-            phonePrices.Add("Samsung Galaxy S24", 899.99); 
-            phonePrices.Add("Google Pixel 8", 799.99); 
-            phonePrices.Add("Xiaomi 14", 699.99); 
-            phonePrices.Add("OnePlus 12", 749.99); 
-            phonePrices.Add("Huawei P60", 649.99); 
-            phonePrices.Add("Nokia G60", 299.99); 
-            phonePrices.Add("Motorola Edge 40", 449.99); 
-            return phonePrices; 
-        }
-        public static double CalculateTotalAmount(int quantity, double price) // Функция вычисления общей суммы
-        {
-            return quantity * price; 
-        }
         public static void AddSale(List<PhoneSale> sales, Dictionary<string, double> phonePrices, DateTime date, string phoneModel, int quantity) // Функция добавления продажи
         {
-            double price = 500.00; // Установка цены по умолчанию
-            if (phonePrices.ContainsKey(phoneModel)) // Проверка наличия модели в словаре цен
-            {
-                price = phonePrices[phoneModel]; 
-            }
-            PhoneSale sale = new PhoneSale(); 
-            sale.Date = date; 
-            sale.PhoneModel = phoneModel; 
-            sale.Quantity = quantity; 
-            sale.Price = price; 
-            sale.TotalAmount = CalculateTotalAmount(quantity, price); // Вычисление общей суммы
+            PhoneSale sale = new PhoneSale();
+            sale.Date = date;
+            sale.PhoneModel = model;
+            sale.Quantity = quantity;
 
-            sales.Add(sale); // Добавление продажи в список
+            // Устанавливаем цену из словаря или по умолчанию
+            if (prices.ContainsKey(model))
+            {
+                sale.Price = prices[model];
+            }
+            else
+            {
+                sale.Price = 500.00; // цена по умолчанию
+            }
+
+            sale.TotalAmount = sale.Quantity * sale.Price;
+            sales.Add(sale);
         }
 
     }
