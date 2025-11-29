7a6fe54f2b78e270aa4c6741a9afdc5ab355b618
st50 <zahark456@gmail.com>
2025-11-29 12:57:54 +0300

    не реализован вывод отчета

---
+++ ManyClassAplication/ManyClassAplication/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static ManyClassAplication.RepoprtRow;
+
+namespace ManyClassAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Goods car1 = new Goods();
+            car1.SetName("Mercedes-Benz");
+            car1.SetPrice(1200000);
+            Goods car2 = new Goods();
+            car2.SetName("УАЗ");
+            car2.SetPrice(10000000);
+
+            Warehouse storage = new Warehouse();
+            storage.SetIdentifier(1);
+            storage.SetLocation("Торжок, Студенческая 3.");
+            storage.SetProductCount(car1, 2);
+            storage.SetProductCount(car2, 5);
+           
+            Console.WriteLine("Адрес склада: " + storage.GetLocation());
+            Console.WriteLine("Идентификатор склада: " + storage.GetIdentifier());
+
+            /*foreach (Goods car in storage.GetProductCount().Keys)
+            {                
+                Console.WriteLine(string.Join(",", storage.GetProductCount()));
+            }*/
+
+            Console.Write("Сумма всех товаров на складе: "); storage.CalculateMoney();
+
+            Console.WriteLine();
+            Console.WriteLine("Название, количество, цена.");
+
+            Report report = new Report();          
+
+            ReportRow row1 = new ReportRow();
+            row1.Product = "Ряженка";
+            row1.Quantity = 5;
+            row1.Price = 100;
+
+            ReportRow row2 = new ReportRow();
+            row2.Product = "Снежок";
+            row2.Quantity = 2;
+            row2.Price = 150;
+
+            ReportRow row3 = new ReportRow();
+            row3.Product = "Сметана";
+            row3.Quantity = 10;
+            row3.Price = 80;
+
+            report.AddReport(row1);
+            report.AddReport(row2);
+            report.AddReport(row3);
+
+            report.PrintReport();
+        }
+    }
+}
---
+++ ManyClassAplication/ManyClassAplication/Report.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ManyClassAplication.RepoprtRow;
+using static ManyClassAplication.Warehouse;
+
+namespace ManyClassAplication
+{
+    class Report
+    {
+        private List<ReportRow> report_ = new List<ReportRow>(); 
+
+        public void AddReport(ReportRow row) 
+        {
+            report_.Add(row);
+        }
+
+        static public string ConvertRowToString(ReportRow row) 
+        {                     
+            return row.Product + ", " + row.Price + ", " + row.Quantity + ".";           
+        }
+        public string PrintReport() 
+        {
+            string result;
+            foreach (ReportRow row in report_)
+            {
+                report_.Add(ConvertRowToString(row));
+            }
+            return result;
+        }
+    }
+}
