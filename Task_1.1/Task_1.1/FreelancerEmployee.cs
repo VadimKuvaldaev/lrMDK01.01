3b55d279ceb346d8e19cb308a646d63c581fea18
st50 <zahark456@gmail.com>
2025-12-03 09:21:15 +0300

    Реализована задача 1.1

---
+++ Task_1.1/Task_1.1/FreelancerEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1
+{
+    public class FreelancerEmployee
+    {
+        private int id_;
+        private string fullName_;
+        private string email_;
+        private decimal projectPayment_;
+        private decimal taxRate_ = 0.13m;
+
+        public FreelancerEmployee(int id, string fullName, string email, decimal payment)
+        {
+            id_ = id;
+            fullName_ = fullName;
+            email_ = email;
+            projectPayment_ = payment;
+        }
+
+        public decimal CalculateSalary()
+        {
+            decimal tax = projectPayment_ * taxRate_;
+            return projectPayment_ - tax;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"ID: {id_}, Имя: {fullName_}, Email: {email_}");
+            Console.WriteLine($"Оплата за проект: {projectPayment_} руб.");
+            Console.WriteLine($"Зарплата (после налога): {CalculateSalary()} руб.");
+        }
+    }
+}
---
+++ Task_1.1/Task_1.1/Role.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1._1
+{
+   public class Role
+    {
+        private string roleName_;
+        private string[] accessibleModules_;
+
+        public Role(string name, string[] modules)
+        {
+            roleName_ = name;
+            accessibleModules_ = modules;
+        }
+
+        public bool HasAccessToModule(string moduleName)
+        {
+            for (int i = 0; i < accessibleModules_.Length; i++)
+            {
+                if (accessibleModules_[i] == moduleName)
+                    return true;
+            }
+            return false;
+        }
+
+        public void Login(string employeeName)
+        {
+            Console.WriteLine($"{employeeName} вошел как {roleName_}");
+        }
+    }
+}
