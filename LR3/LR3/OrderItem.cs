a04eeb88a0ef4fb326715823d5dce6f3e2571ee4
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-01-30 20:37:50 +0300

    Реализован класс OrderItem

--- LR3/LR3/OrderItem.cs
+++ LR3/LR3/OrderItem.cs
@@ -7,6 +7,25 @@ using System.Threading.Tasks;
 namespace LR3
 {
     public class OrderItem
-    {        
+    {
+        private Drugs drug_;
+        private int quantity_;
+        public Drugs Drug
+        {
+            get { return drug_; }
+            set { drug_ = value; }
+        }
+        public int Quantity
+        {
+            get { return quantity_; }
+            set { quantity_ = value; }
+        }
+        public double TotalPrice
+        {
+            get
+            {
+                return drug_ != null ? drug_.Price * quantity_ : 0;
+            }
+        }
     }
 }
