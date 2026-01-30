56660252b81b6e849cd04d4216160084404f5aff
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-01-30 20:33:59 +0300

    Реализован класс Group

--- LR3/LR3/Group.cs
+++ LR3/LR3/Group.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace LR3
 {
     public class Group
-    {        
+    {
+        private string name_;
+        private List<Drugs> drugs_;
+        public Group(string name)
+        {
+            name_ = name;
+            drugs_ = new List<Drugs>();
+        }
+        public string Name
+        {
+            get { return name_; }
+        }
+        public List<Drugs> Drugs
+        {
+            get { return drugs_; }
+        }
+        public void AddDrug(Drugs drug)
+        {
+            if (drug != null)
+            {
+                drugs_.Add(drug);
+            }
+        }
     }
 }
