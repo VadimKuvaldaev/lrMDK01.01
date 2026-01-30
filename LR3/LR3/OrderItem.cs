498ff7d4766305229ef55e48887a2e49a750f308
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-01-30 20:23:10 +0300

    Создана форма, добавлены классы Drugs, Group и OrderItem

---
+++ LR3/LR3/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3
+{
+    public class OrderItem
+    {        
+    }
+}
