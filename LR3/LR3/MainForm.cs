a327224f003fd2a90b333fa15e2b945eaefee62e
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-02-02 17:39:47 +0300

    добавлены две группы лекарств: антибиотики и витамины, также в них было внесено по 2 записи

--- LR3/LR3/MainForm.cs
+++ LR3/LR3/MainForm.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace LR3
 {
+    
     public partial class MainForm : Form
     {
+        private Dictionary<string, List<Drugs>> drugs_ = new Dictionary<string, List<Drugs>>();
         public MainForm()
         {
             InitializeComponent();
+
+            drugs_.Add("Антибиотики", 
+                new List<Drugs>() { 
+                    new Drugs("Антигриппин", 199.99, "Фарма", new DateTime(2028, 07, 01), "Аптека.ru"),
+                    new Drugs("Детралекс", 149.99, "Амбробене", new DateTime(2026, 11, 20), "Форте")
+                }
+            );
+            drugs_.Add("Витамины",
+                new List<Drugs>() {
+                    new Drugs("Витамишки", 289.89, "Фармацевт", new DateTime(2026, 07, 15), "Аптека.ru"),
+                    new Drugs("Аскорбиновая кислота", 69.49, "Фарма", new DateTime(2027, 01, 05), "Озон")
+                }
+            );
         }
     }
 }
