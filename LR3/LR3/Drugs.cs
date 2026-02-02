91cf089d9f28e25d81ec12dd0fc3b1773bfb0e40
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-02-02 17:25:25 +0300

    Реализован класс Drugs

---
+++ LR3/LR3/Drugs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3
+{
+    public class Drugs
+    {
+        private string name_;
+        private double price_;
+        private string manufacturer_;
+        private DateTime shelfLifeDate_;
+        private string provide_;
+        public Drugs(string name, double price, string manufacturer, DateTime date, string provider)
+        { 
+            name_ = name;
+            price_ = price;
+            manufacturer_ = manufacturer;
+            shelfLifeDate_ = date;
+            provide_ = provider;           
+        }
+        public string Name 
+        {
+            get { return name_; }
+        }
+    }
+}
