6c00c12fc45498eda07591509f0b115823f0e3d7
qqniqq <mstitel2025@gmail.com>
2026-02-03 11:20:14 +0300

    Доделать дома и в интерфейсе реализовать метод "Авторизация"

--- TestFileStorage/TestFileStorage/MainForm.cs
+++ TestFileStorage/TestFileStorage/MainForm.cs
@@ -12,11 +12,37 @@ namespace TestFileStorage
 {
     public partial class MainForm : Form
     {
+        List<string> allCategories = drugs_.Keys.ToList();
         public MainForm()
         {
             InitializeComponent();
+            List<string> allLogins = .Keys.ToList();
+            LoginComboBox.DataSource = ;
         }
 
-        
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            //string login = LoginTextBox.Text;
+            //string password = PasswordTextBox.Text;
+            List<User> users = new List<User>();
+            FileUsersStorage fileUsersStorage = new FileUsersStorage();
+            users = fileUsersStorage.Load();
+            foreach (User data in users)
+            {
+                if (PasswordTextBox.Text == data.Password)
+                {
+                    MessageBox.Show("Пароль введен верно :)");
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Пароль введен неверно :(");
+                    return;
+                }
+            } 
+        }
+
+       
     }
 }
