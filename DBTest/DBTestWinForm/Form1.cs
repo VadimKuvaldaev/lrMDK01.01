cad6147b658a07231e62844da9efc56139dc3fae
Vadim Kuvaldaev <vadimkuvaldaev@gmail.com>
2026-03-17 11:17:49 +0300

    Реализован проект, в котором берутся данные из бд и выводятся в datagridview

---
+++ DBTest/DBTestWinForm/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace DBTestWinForm
+{
+    public partial class Form1: Form
+    {
+        
+        
+        public Form1()
+        {                                              
+            InitializeComponent();
+            PgUsersLoader loader = new PgUsersLoader();
+            List<User> users = loader.Load();
+            dataGridView.DataSource = users;
+        }
+    }
+}
---
+++ DBTest/DBTestWinForm/PgUsersLoader.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBTestWinForm
+{
+    public class PgUsersLoader
+    {
+        public List<User> Load() 
+        {
+            List<User> result = new List<User>();            
+            var cs = "Host=192.168.1.48;Username=st50-10;Password=5010;Database=MDK01.01_Kuvaldaev";
+            var con = new NpgsqlConnection(cs);
+            con.Open();
+            var sql = "SELECT login, password, age, name, last_name From myusers";
+            var cmd = new NpgsqlCommand(sql, con);
+            var reader = cmd.ExecuteReader();           
+            while (reader.Read())
+            {
+                User user = new User
+                {
+                   Login = reader.GetString(0),
+                   Password = reader.GetString(1),
+                   Age = reader.GetInt32(2),
+                   Name = reader.GetString(3),
+                   LastName = reader.GetString(4)
+                };
+                result.Add(user);
+            }
+            return result;
+        }               
+    }
+}
